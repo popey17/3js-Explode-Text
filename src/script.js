2eb2d9a5db7ca784801c288724e449c747351d90
aungmyatkyaw <contact.popey17@gmail.com>
2024-03-09 21:01:48 +0630

    update name

--- src/script.js
+++ src/script.js
@@ -55,7 +55,7 @@ fontLoader.load(
             }
         )
         const textGeometry2 = new TextGeometry(
-            'by @leoChen',
+            'by @LeoChen',
             {
                 font: font,
                 size: 0.7,
